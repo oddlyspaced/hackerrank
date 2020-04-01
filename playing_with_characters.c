776dcf87713931e9ce9abc2a3e59cbdabf9e9742
oddlyspaced <oddlyspaced1@gmail.com>
2020-04-01 10:52:38 +0530

    Add playing with characters problem

---
+++ playing_with_characters.c
@@ -0,0 +1,13 @@
+// https://www.hackerrank.com/challenges/playing-with-characters/problem
+#include<stdio.h>
+int main(){
+    char ch[50];
+    char s[50];
+    char sen[50];
+    gets(ch);
+    gets(s);
+    gets(sen);
+    puts(ch);
+    puts(s);
+    puts(sen);
+}
